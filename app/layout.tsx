b69942e23d637d3440a71c2bcc830b6bcc0a1406
Biljana Kotevska <biljanakotev@gmail.com>
2023-11-24 10:38:35 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Travel',
@@ -12,6 +13,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
+        <Head>
+          <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+          {/* <meta name='viewport' content='width=device-width, initial-scale=1.0' /> */}
+        </Head>
         <Navbar />
         <main className='relative overflow-hidden'> {children}</main>
         <Footer />
